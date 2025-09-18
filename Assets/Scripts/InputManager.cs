05ee4a53724685905a3d9be23e15ae80af27c82b
RJOB SAMY <samy_rjob@proton.me>
2025-09-18 11:34:29 +0100

    Camera rotation

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -8,12 +8,16 @@ public class InputManager : MonoBehaviour
     private PlayerInput.OnFootActions onFoot;
     private PlayerMotor motor;
 
+    private PlayerLook look;
+
 
     private void Awake()
     {
         playerInput = new PlayerInput();
         onFoot = playerInput.OnFoot;
         motor = GetComponent<PlayerMotor>();
+        look = GetComponent<PlayerLook>();
+        onFoot.Jump.performed += ctx => motor.jump();
     }
 
     //private void FixedUpdate()
@@ -25,6 +29,11 @@ public class InputManager : MonoBehaviour
     private void Update()
     {
         motor.processMove(onFoot.Movement.ReadValue<Vector2>());
+    }
+
+    private void LateUpdate()
+    {
+        look.ProcessLook(onFoot.Look.ReadValue<Vector2>());
     }
 
     private void OnEnable()
